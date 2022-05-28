df98d08b4aac5262f416263137db975fb2c8ee5d
Sallykamel <106214987+Sallykamel@users.noreply.github.com>
2022-05-28 17:04:36 +0200

    Add files via upload

---
+++ Engine/src/Main.c4.scala
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+
+object Main {
+
+ def main(args: Array[String]): Unit = {
+   var draw = new Drawer
+   draw.Drawer()
+   var control = new Controller
+   var player = control.turn
+   var flag = false // true if invalid input
+
+   val action = new AbstractAction() {
+     override def actionPerformed(e: ActionEvent): Unit = {
+       control.input = (draw.textBox.getText).charAt(0)//input
+       if (!control.input_Validation(control.input) || !((draw.textBox.getText).length==1) ){
+         println("invalid input! you should enter an alphapet from a to g")
+         flag = true
+       }
+       else{
+           var myBoard = control.board
+           var col = control.convToNum(control.input)
+           var row = control.rowPosition(control.convToNum,control.input,myBoard)
+           if(row != 10){
+             player= draw.play(player,row, col)
+             println("VALID")
+           }
+           else{
+             flag = true
+             println("invalid input! the column is full")
+           }
+       }
+       draw.textBox.setText("")
+     }
+   }
+   draw.textBox.addActionListener(action)
+ }
+}
